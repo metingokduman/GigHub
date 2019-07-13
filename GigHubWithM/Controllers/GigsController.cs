2f998a449b39c621fde18dded88ac1e9aba2bc71
metingokduman <50114043+metingokduman@users.noreply.github.com>
2019-07-13 15:52:48 +0300

    Implement Validation

--- GigHubWithM/Controllers/GigsController.cs
+++ GigHubWithM/Controllers/GigsController.cs
@@ -29,10 +29,17 @@ namespace GigHubWithM.Controllers
         [HttpPost]
         public ActionResult Create(GigFormViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("Create", viewModel);
+            }
+                
+                        
             var gig = new Gig
             {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = viewModel.DateTime,
+                DateTime = viewModel.GetDateTime(),
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
             };
--- GigHubWithM/ViewModels/GigFormViewModel.cs
+++ GigHubWithM/ViewModels/GigFormViewModel.cs
@@ -1,24 +1,33 @@
 ﻿using GigHubWithM.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHubWithM.ViewModels
 {
     public class GigFormViewModel
     {
-       public DateTime DateTime
-        {
-            get
-            {
-                return DateTime.Parse(string.Format("{0} {1}", Date, Time));
-            }
 
+        [Required]
+        public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+
+        [Required]
+        public byte Genre { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
         }
 
-        public string Venue { get; set; }
-        public string Date { get; set; }
-        public string Time { get; set; }
-        public byte Genre { get; set; }
-        public IEnumerable<Genre> Genres { get; set; }
     }
 }
