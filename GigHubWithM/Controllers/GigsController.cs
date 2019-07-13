28a5d8494cfbc847f51caa3670d14cb2c0d06313
metingokduman <50114043+metingokduman@users.noreply.github.com>
2019-07-13 13:07:55 +0300

    Create Add a Gig From

---
+++ GigHubWithM/Controllers/GigsController.cs
@@ -0,0 +1,25 @@
+﻿using GigHubWithM.Models;
+using GigHubWithM.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHubWithM.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewmodel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewmodel);
+        }
+    }
+}
---
+++ GigHubWithM/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GigHubWithM.Models;
+using System.Collections.Generic;
+
+namespace GigHubWithM.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
