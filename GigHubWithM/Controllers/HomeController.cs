88fa3fb687fed2798f60859dee1db79c65b51eeb
metingokduman <50114043+metingokduman@users.noreply.github.com>
2019-07-15 15:44:51 +0300

    Improve the look and feel of the home page.

--- GigHubWithM/Controllers/HomeController.cs
+++ GigHubWithM/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace GigHubWithM.Controllers
         {
             var upcomingGigs = _context.Gigs
                 .Include(g => g.Artist)
+                .Include(g=>g.Genre)
                 .Where(g => g.DateTime < DateTime.Now).ToList();
 
             return View(upcomingGigs);
